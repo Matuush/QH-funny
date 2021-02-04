fa1ed5e2ab19ea006896153bb50ed4f3a5fad02d
Matuush <56359492+Matuush@users.noreply.github.com>
2021-02-04 23:57:24 +0100

    prádel
    bottom text

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import Discord from 'discord.js';
 import colors from 'colors';
 import { Sensitive } from './types.d';
 colors;
-const sensitive: Sensitive = JSON.parse(fs.readFileSync(path.join(__dirname, `./sensitive.json`)).toString());
+var sensitive: Sensitive = JSON.parse(fs.readFileSync(path.join(__dirname, `./sensitive.json`)).toString());
 const TOKEN = sensitive.token;
 
 const client = new Discord.Client();
@@ -12,10 +12,14 @@ const client = new Discord.Client();
 client.on("ready", () => {
     console.log("I AM READY!".red);
 });
-client.on("message", msg => {
-    if (sensitive.userIDs.includes(msg.author.id)) {
+client.on("message", async msg => {
+    try{sensitive = JSON.parse(fs.readFileSync(path.join(__dirname, `./sensitive.json`)).toString());}
+    catch(e){}
+    if(msg.author.bot) return;
+    if (Object.keys(sensitive.responses).find(key => msg.content.includes(key))) return await msg.channel.send(sensitive.responses[Object.keys(sensitive.responses).find(key => msg.content.includes(key)) || "pardel"]);
+    if (sensitive.userIDs.includes(msg.author.id) || msg.channel.type == "dm") {
         const index = Math.round(Math.random() * (sensitive.messages.length - 1));
-        msg.channel.send(sensitive.messages[index]);
+        await msg.channel.send(sensitive.messages[index]);
     }
 });
 client.login(TOKEN);
--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,8 @@
+import { AnyARecord } from "dns";
+
 export interface Sensitive{
     token: string;
     userIDs: Array<string>;
     messages: Array<string>;
+    responses: any;
 }
